b1bbb783beb25266c5a19beb4ae9ed5d62209a03
TomTom2019 <daless2000be@yandex.com>
2022-10-29 14:46:49 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const app = express();
+
+const mongoUri = `mongodb+srv://admin:testing123@cluster0.lwqgg.mongodb.net/MyApp?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri);
+
+/// MIDDLEWARE
+app.use(bodyParser.json());
+
+
+/// MODELS
+const { User } = require('./models/user');
+
+
+/// Routes
+
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save((err,doc)=>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(doc)
+    })
+})
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`started on port ${port}`)
+})
