a95fcf6be4a3c3d7ee87b56d6c507c24c339009b
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-07-22 15:21:57 +0200

    hash update

---
+++ hash.js
@@ -0,0 +1,11 @@
+// bcrypt => generate salt => ramdom hash
+const bcrypt = require('bcrypt');
+
+bcrypt.genSalt(10,(err,salt)=>{
+	/*console.log(salt)*/
+    if(err) return false;
+    bcrypt.hash('password123',salt,(err,hash)=>{
+        if(err) return false;
+        console.log(hash)
+    })
+})
