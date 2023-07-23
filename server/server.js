ca1c24ce4f8e4ad3f08bc9658cce77356ce6f3b1
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-07-23 14:12:43 +0200

    update

--- server/server.js
+++ server/server.js
@@ -19,9 +19,26 @@ app.use(bodyParser.json());
 const { User } = require('./models/user');
 
 
+/* ROUTE
+Postman :http://localhost:3001/api/user  reasponse json =>{
+    "email":"Pipo@gmail.com",
+    "password":"test123"
+}*/
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save((err,doc)=>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(doc)
+    })
+})
 
 
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`started on port ${port}`)
 })
