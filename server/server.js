13d5e942c886b3265719b7df4cdfb36d73075cf0
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-07-25 16:59:19 +0200

    update

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const app = express();
+const bcrypt = require('bcrypt');
 
 
 // connect to database
@@ -19,9 +20,46 @@ app.use(bodyParser.json());
 const { User } = require('./models/user');
 
 
+/* ROUTE
+Postman :http://localhost:3001/api/user  reasponse json =>{
+    "email":"Pipo@gmail.com",
+    "password":"test123"
+}*/
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    user.save((err,doc)=>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(doc)
+    })
+})
+
+// ROUTE REGERISTER USER
+app.post('/api/user/login',(req,res)=>{
+    // find user if good => move foward
+    User.findOne({'email': req.body.email},(err,user)=>{
+        if(err) res.status(400).send(err);
+        if(!user) res.json({message:'User not found'})
+  
+     // 2 - compare the password with the HASHED password on the DB, -> 
+   user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(err) res.status(400).send(err);
+            if(!isMatch) res.json({message:'Bad password'})
+
+                res.status(200).send(isMatch)
+
+            })
+              
+        })
+    })
 
 
-const port = process.env.PORT || 3000;
+
+
+const port = process.env.PORT || 3001;
 app.listen(port,()=>{
     console.log(`started on port ${port}`)
 })
